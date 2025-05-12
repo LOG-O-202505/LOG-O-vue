301eaefd54b11ab27cf1534a85ae49865d09470b
qudcks8084 <qudcks8084@naver.com>
2025-05-12 11:32:35 +0900

    [ADD] 카카오 API 추가

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ export default {
     ES_API: process.env.VUE_APP_ES_API,
     MODEL_NAME: process.env.VUE_APP_MODEL_NAME,
     GOOGLE_MAPS_API_KEY: process.env.VUE_APP_GOOGLE_MAPS_API_KEY ,
+    KAKAO_MAPS_API_KEY: process.env.VUE_APP_KAKAO_MAPS_API_KEY,
     BASE_URL: process.env.VUE_APP_BASE_URL
   }
